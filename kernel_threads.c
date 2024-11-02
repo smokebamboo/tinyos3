48e5c474db7192616215986bb447a63ad99f2406
taconoodle <giannisvlassis1@gmail.com>
2024-11-02 06:47:05 +0200

    Add create, return-self, join-thread for multithreading

--- kernel_threads.c
+++ kernel_threads.c
@@ -2,13 +2,30 @@
 #include "tinyos.h"
 #include "kernel_sched.h"
 #include "kernel_proc.h"
-
+#include "kernel_cc.h"
 /** 
   @brief Create a new thread in the current process.
   */
 Tid_t sys_CreateThread(Task task, int argl, void* args)
 {
-	return NOTHREAD;
+  /*Create a TCB and a PTCB and initialize them*/
+  TCB* tcb = spawn_thread(CURPROC, start_thread);
+  PTCB* ptcb = init_ptcb(task, argl, args);
+
+  /*Connect ptcb and tcb together*/
+  tcb->ptcb = ptcb;
+  ptcb->tcb = tcb;
+
+  /*
+    Push the ptcb at the tail of the PTCB list of the current process
+    and increment the counter of the amount of threads belonging to it
+  */
+  rlist_push_back(&CURPROC->ptcb_list, &ptcb->ptcb_list_node);
+  CURPROC->thread_count++;
+  /*START RUNNING*/
+  wakeup(tcb);
+
+  return (Tid_t) ptcb;
 }
 
 /**
@@ -23,8 +40,25 @@ Tid_t sys_ThreadSelf()
   @brief Join the given thread.
   */
 int sys_ThreadJoin(Tid_t tid, int* exitval)
-{
-	return -1;
+{ 
+  /*Check to see thread exists*/
+  rlnode* tempnode = rlist_find(&cur_thread()->owner_pcb->ptcb_list, (PTCB*) tid, NULL);
+	if (tempnode == NULL) return -1;
+
+  PTCB* joinedptcb = tempnode->ptcb;
+
+  if (joinedptcb->detached || joinedptcb == CURPTCB) return -1;
+  /*hawk tuah wait on that thang*/
+  joinedptcb->refcount++;
+  while(!(joinedptcb->exited || joinedptcb->detached)) {
+    kernel_wait(&joinedptcb->exit_cv, SCHED_USER);
+  }
+  joinedptcb->refcount--;
+
+  /*return the exit value*/
+  if (joinedptcb->exitval) (*exitval = joinedptcb->exitval);
+
+  return 0;
 }
 
 /**
